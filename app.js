562c7a50c4fac02a86893de2e60da1a66203cfd1
nikhil-222 <141047008+nikhil-222@users.noreply.github.com>
2024-02-24 17:17:14 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+let chartContainer = document.querySelector('.chart_container');
+let submitBtn = document.querySelector(".submit_btn");
+
+
+submitBtn.addEventListener("click", (e) => {
+    chartContainer.style.display = 'block';
+});
